87208d97be27cb94cb3f8f21c073d65da772e9c5
6822846 <149351577+6822846@users.noreply.github.com>
2023-11-08 03:43:01 -0800

    frontend code added first time

---
+++ frontend/front-end/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom"
+// import Books from './components/Books';
+
+
+export default function App() {
+  return (
+    
+    <div >
+      {/* <Routes>
+        <Route path="app/books" element={<Books />} />
+      </Routes> */}
+    </div>
+  );
+}
+
+ 
