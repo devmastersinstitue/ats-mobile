22eccc2f317ab29b94c2ea0ffeaea7e511d59b03
6822846 <149351577+6822846@users.noreply.github.com>
2023-11-09 07:31:12 -0800

    resolve react app starting issue

--- frontend/front-end/src/App.js
+++ frontend/front-end/src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes, Navigate} from "react-router-dom"
-// import Books from './components/Books';
-
+import React from 'react';
 
 export default function App() {
   return (
     
     <div >
-      {/* <Routes>
-        <Route path="app/books" element={<Books />} />
-      </Routes> */}
+      <h2>Heading</h2>
+    
     </div>
   );
 }
--- frontend/front-end/src/index.js
+++ frontend/front-end/src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import Books from './components/Books'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from 'react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>    
-  </BrowserRouter>
-  // <Books />, document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <React.StrictMode>
+//       <App/>
+//     </React.StrictMode>    
+//   </BrowserRouter>
+//   // <Books />, document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
