1bf46fad4d6cbe00c5120851ed02b2d0d4b64511
kashmala <kkhankkhan009@gmail.com>
2023-11-11 17:07:20 +0500

    added props

--- frontend/front-end/src/App.js
+++ frontend/front-end/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 
 import React, { useState, useEffect } from "react";
 
-export default function App() {
+export default function App(props) {
   
 
   return (
     
     <div >
       <h2>Heading</h2>
-    
+      <h1>{props.name}</h1>
+      <h3>{props.fatherName}</h3>
+
     </div>
   );
 }
--- frontend/front-end/src/index.js
+++ frontend/front-end/src/index.js
@@ -11,19 +11,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App/>
+  <App name="kashmala" fatherName= "Aziz-ur-rehman"/>
   </React.StrictMode>
 );
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <React.StrictMode>
-//       <App/>
-//     </React.StrictMode>    
-//   </BrowserRouter>
-//   // <Books />, document.getElementById('root')
-// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
