91196057ab8bf788ceb8bf4d55e22cc88955ee6f
6822846 <149351577+6822846@users.noreply.github.com>
2023-11-11 04:35:24 -0800

    Successfully added tailwindcss configuration

--- frontend/front-end/src/App.js
+++ frontend/front-end/src/App.js
@@ -9,7 +9,7 @@ export default function App(props) {
   return (
     
     <div >
-      <h2>Heading</h2>
+      <h2 className="text-3xl font-bold underline text-red-800">Heading</h2>
       <h1>{props.name}</h1>
       <h3>{props.fatherName}</h3>
 
