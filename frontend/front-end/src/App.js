d00d0a8b50ea3f2d1c819df6021794ab985e4209
kashmala <kkhankkhan009@gmail.com>
2023-11-13 17:37:54 +0500

    learn function

--- frontend/front-end/src/App.js
+++ frontend/front-end/src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 
 import React, { useState, useEffect } from "react";
 
-export default function App(props) {
-  
-
+function UserInfo(props){
   return (
-    
-    <div >
-      <h2 className="text-3xl font-bold underline text-red-800">Heading</h2>
-      <h1>{props.name}</h1>
-      <h3>{props.fatherName}</h3>
+  <div className='flex flex-row border-blue-500 border-2 justify-between m-10 py-5'>
+    <p>Name : {props.name}</p>
+    <p>Father Name : {props.fatherName}</p>
+    <p>Age : {props.age}</p>
+    <p>Email : {props.email}</p>
+  </div>
+  );
+}
 
+export default function App(props) {
+    return (
+    <div >
+      <UserInfo name ="KASHMALA" fatherName ="Aziz-ur-Rehman" age ="18" email ="kkhankkhan009@gmail.com"/>
+      <UserInfo name ="Mala" fatherName ="Ali" age ="23" email ="malakhan009@gmail.com"/>
     </div>
   );
 }
 
  
+{/* <h2 className="text-5xl font-bold underline text-blue-800 my-8
+       m-8">
+       Promotion of tree plantation on state and private land</h2>
+      <h1 className="text-4xl font-bold text-red-700 p-8">  { props.name }</h1>
+      <h3 className='text-2xl font-bold m-8'>{props.topic}</h3> */}
--- frontend/front-end/src/index.js
+++ frontend/front-end/src/index.js
@@ -11,7 +11,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App name="kashmala" fatherName= "Aziz-ur-rehman"/>
+  <App/>
   </React.StrictMode>
 );
+
 
