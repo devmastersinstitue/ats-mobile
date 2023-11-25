6903fde862c35cddce97222bfd18867828feff98
6822846 <149351577+6822846@users.noreply.github.com>
2023-11-25 20:41:51 +0500

    using table code

--- frontend/front-end/src/Component/Student.jsx
+++ frontend/front-end/src/Component/Student.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React from "react";
 
 class Student extends React.Component {
-    render() {
+  render() {
+    return (
+      <div>
+        <div className=" bg-red-400 mx-40 my-10 p-10 text-center text-green-600">
+          <h2>Hi, i am a student</h2>
+        </div>
+        <div className="container mx-auto mt-5">
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                  Name
+                </th>
+                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                  CNIC
+                </th>
+                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                  Contact Number
+                </th>
+                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                  Fees
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.students.map((student) => (
+                <tr className="text-center">
+                  <td className="py-2 px-4 border-b text-sm text-gray-600">
+                    {student.human.name}
+                  </td>
+                  <td className="py-2 px-4 border-b text-sm text-gray-600">
+                    {student.human.cnic}
+                  </td>
+                  <td className="py-2 px-4 border-b text-sm text-gray-600">
+                    {student.human.contactNo}
+                  </td>
+                  <td className="py-2 px-4 border-b text-sm text-gray-600">
+                    {student.fees}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
        
-        return (
-            <div>
-                <div className=' bg-red-400 mx-40 my-10 p-10 text-center text-green-600' >
-                    <h2>Hi, i am a student</h2>
-                </div>
-                <div>
-                    <div className='flex flex-row justify-between px-40'>
-                        <div className='w-20'>Name</div>
-                        <div className='w-40'>CNIC</div>
-                        <div className='w-40'>Contact Number</div>
-                        <div className='w-20'>Fees</div>
-                    </div>
-                    {this.props.students.map((student) => (
-                        <div className='flex flex-row justify-between px-40 border'>
-                            <div className='w-20'>{student.human.name}</div>
-                            <div className='w-40'>{student.human.cnic}</div>
-                            <div className='w-40'>{student.human.contactNo}</div>
-                            <div className='w-20'>{student.fees}</div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
-    }
-};
+      </div>
+    );
+  }
+}
 
 export default Student;
