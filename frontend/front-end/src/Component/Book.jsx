0ed46568c9c3cd0e3b4012de665bd34305c0c901
kashmala <kkhankkhan009@gmail.com>
2023-11-26 15:08:17 +0500

    using table method

--- frontend/front-end/src/Component/Book.jsx
+++ frontend/front-end/src/Component/Book.jsx
@@ -1,29 +1,51 @@
-import React  from 'react';
+import React from "react";
 
-function Book  (props) {
-    return (
-        <div>
-            <div className=' bg-red-400 mx-40 my-10 p-10 text-center' >
-                    <h2>Book Function</h2>
-                </div>
-                <div>
-                    <div className='flex flex-row justify-between px-40'>
-                        <div>Book Name</div>
-                        <div>Color</div>
-                        <div>Price</div>
-                        <div>Author</div>
-                    </div>
-                    {props.books.map((book) => (
-                        <div className='flex flex-row justify-between px-40 border'>
-                            <div>{book.name}</div>
-                            <div>{book.color}</div>
-                            <div>{book.price}</div>
-                            <div>{book.author.name}</div>
-                        </div>
-                    ))}
-                </div>
-        </div>
-    )
+function Book(props) {
+  return (
+    <div>
+      <div className=" bg-blue-400 mx-30 my-30 p-5 text-center">
+        <h2>Book Function</h2>
+      </div>
+      <div className="container mx-auto mt-5">
+        <table className="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr className="bg-blue-200">
+              <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600 ">
+                Book Name
+              </th>
+              <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
+                Color
+              </th>
+              <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
+                Price
+              </th>
+              <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody class="[&>*:nth-child(even)]:bg-gray-100 [&>*:nth-child(odd)]:bg-gray-300">
+            {props.books.map((book) => (
+              <tr className="text-center">
+                <td className="py-2 px-4 border-b text-sm text-gray-600">
+                  {book.name}
+                </td>
+                <td className="py-2 px-4 border-b text-sm text-gray-600">
+                  {book.color}
+                </td>
+                <td className="py-2 px-4 border-b text-sm text-gray-600">
+                  {book.price}
+                </td>
+                <td className="py-2 px-4 border-b text-sm text-gray-600">
+                  {book.author.name}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
--- frontend/front-end/src/Component/Student.jsx
+++ frontend/front-end/src/Component/Student.jsx
@@ -4,28 +4,28 @@ class Student extends React.Component {
   render() {
     return (
       <div>
-        <div className=" bg-red-400 mx-40 my-10 p-10 text-center text-green-600">
+        <div className=" bg-blue-400 mx-30 my-30 p-5 text-center text-black-600">
           <h2>Hi, i am a student</h2>
         </div>
         <div className="container mx-auto mt-5">
           <table className="min-w-full bg-white border border-gray-300">
             <thead>
-              <tr>
-                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+              <tr className="bg-blue-200">
+                <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
                   Name
                 </th>
-                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
                   CNIC
                 </th>
-                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
                   Contact Number
                 </th>
-                <th className="py-2 px-4 border-b font-bold uppercase text-sm text-gray-600">
+                <th className="py-2 px-4 border-b font-bold uppercase text-lg text-gray-600">
                   Fees
                 </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody class="[&>*:nth-child(even)]:bg-gray-100 [&>*:nth-child(odd)]:bg-gray-300">
               {this.props.students.map((student) => (
                 <tr className="text-center">
                   <td className="py-2 px-4 border-b text-sm text-gray-600">
